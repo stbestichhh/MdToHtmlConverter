b724af15bd3c43ca343439efc87dc32ba49c18da
Kiril Yakymchuk <stbestich@gmail.com>
2024-03-05 13:55:02 +0200

    Created converter

--- converter.js
+++ converter.js
@@ -1,4 +1,13 @@
+const fs = require('fs');
+
 class Converter {
+  constructor(mdFile, htmlFile) {
+    this.mdFile = mdFile;
+    this.htmlFile = htmlFile;
+  }
+
+  #preformatedText = [];
+
   #patterns = [
     {
       regexp: /(?<=[ ,.:;\n\t]|^)\*\*(?=\S)(.+?)(?<=\S)\*\*(?=[ ,.:;\n\t]|$)/g,
@@ -13,6 +22,49 @@ class Converter {
       html: '<tt>$1</tt>',
     },
   ];
+
+  convert() {
+    fs.readFile(this.mdFile, 'utf8', (err, data) => {
+      if (err) {
+        throw new Error('Cannot read file');
+      }
+
+      const formattedText = this.#getPreformatedText(data);
+      const html = this.#patterns.reduce((prev, cur) => {
+        prev.replace(cur.regexp, cur.html);
+      }, formattedText);
+
+      const paragraphs = this.#getParagraphs(html);
+      console.log(this.#setPreformattedText(paragraphs));
+    });
+  }
+
+  #getPreformatedText(text) {
+    const preformattedPattern = /```([\s\S]*?)```/g;
+    const preformattedText = text.match(preformattedPattern);
+
+    if (!preformattedText) {
+      return text;
+    }
+
+    this.#preformatedText.push(...preformattedText);
+    return preformattedText.reduce((prev, cur, curIndex) => {
+      return prev.replace(cur, `PRE{{${curIndex}}}PRE`)
+    }, text);
+  }
+
+  #setPreformattedText(text) {
+    return this.#preformatedText.reduce((prev, cur, curIndex) => {
+      const html = `<pre>${cur.replace(/```/g, '')}</pre>`;
+      return prev.replace(`PRE{{${curIndex}}}PRE`, html);
+    }, text);
+  }
+
+  #getParagraphs(text) {
+    return text.split('\n\n').reduce((prev, cur) => {
+      return `${prev}\n<p>${cur}</p>`;
+    }, '');
+  }
 }
 
 module.exports = Converter;
