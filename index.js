265cc066d9050c0ad2a16a3c8d92cacff4e87197
Kiril Yakymchuk <stbestich@gmail.com>
2024-03-05 14:25:02 +0200

    Run app

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+const Converter = require('./converter.js');
+
 if (process.argv.length < 3) {
   console.error('Usage: node index.js <inputMdFilePath> [--out <outputHtmlFilePath>]');
   process.exit(1);
 }
+
+const mdfilePath = process.argv[2];
+const htmlfileKey = process.argv.indexOf('--out');
+const htmlfilePath = htmlfileKey !== -1 ? process.argv[htmlfileKey + 1] : null;
+
+const converter = new Converter(mdfilePath, htmlfilePath);
+converter.convert();
