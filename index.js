e4d04ffdcb9b828777b3ba86ffc1953dd501e2dc
Kiril Yakymchuk <stbestich@gmail.com>
2024-03-09 11:58:46 +0200

    Added doctype to result

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const Converter = require('./converter.js');
 
 if (process.argv.length < 3) {
-  console.error('Usage: node index.js <inputMdFilePath> [--out <outputHtmlFilePath>]');
+  console.error('Usage: node index.js <inputMdFilePath> [--dt] [--out <outputHtmlFilePath>]');
   process.exit(1);
 }
 
 const mdfilePath = process.argv[2];
 const htmlfileKey = process.argv.indexOf('--out');
 const htmlfilePath = htmlfileKey !== -1 ? process.argv[htmlfileKey + 1] : null;
+const docTypeFlag = process.argv.indexOf('--dt');
 
 const converter = new Converter(mdfilePath, htmlfilePath);
-converter.convert();
+converter.convert(docTypeFlag);
