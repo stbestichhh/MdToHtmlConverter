ea9eff47bde648c7352f12b8bfba9af680a92112
Kiril Yakymchuk <stbestich@gmail.com>
2024-03-11 11:25:34 +0200

    Check syntax tests

--- src/checkSyntax.spec.js
+++ src/checkSyntax.spec.js
@@ -10,22 +10,15 @@ describe('Check syntax function', () => {
     expect(() => checkSyntax(line)).toThrow(errorMessage);
   });
 
+  it('Should pass if line starts with `_ `', () => {
+    const line = '_ something';
+    expect(checkSyntax(line));
+  });
+
   it('Should throw if line has wrong syntax', () => {
     const line = '**`_something_`**';
     const errorMessage = line + ' | Invalid input: Lines with the pattern **_$1_** are not allowed.';
     expect(() => checkSyntax(line)).toThrow(errorMessage);
   });
+
 });
-
-// const checkSyntax = (text) => {
-//   const lines = text.split('\n');
-//   for (const line of lines) {
-//     if (line.startsWith('_') && !line.endsWith('_') && !line.startsWith('_ ')) {
-//       throw new Error(line + ' | Invalid input: Lines starting with _ followed by a word are not allowed.');
-//     } else if (/\*\*`_([^`]+?)_`\*\*/.test(line)) {
-//       throw new Error(line + ' | Invalid input: Lines with the pattern **_$1_** are not allowed.');
-//     }
-//   }
-// }
-
-// module.exports = checkSyntax;
